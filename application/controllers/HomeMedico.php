adb28e72896b8f5e95cb53012d884e55afe2da6a
HugoF <HugoF@Hugo>
2014-04-17 19:50:00 +0100

    hugo

---
+++ application/controllers/HomeMedico.php
@@ -0,0 +1,16 @@
+<?php
+
+class HomeMedico extends CI_Controller{
+    
+    public function index() {
+       
+        $this->load->view('HomeMedico');
+    }
+    
+    function __construct() {
+        parent::__construct();
+        //para formularios $this->load->helper(form);
+         $this->load->helper('url');
+    }
+}
+?>
