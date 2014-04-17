c2a8496666342fd6b89a99cf7f7de6a9663a4ebb
HugoF <HugoF@Hugo>
2014-04-17 19:43:36 +0100

    hugo2

---
+++ application/views/HomeMedico.php
@@ -0,0 +1,52 @@
+<?php  $this->load->view('Head');?>
+ 
+<h1 class="titulo">Pagina Pessoal</h1>
+
+<div id="contentor">
+    
+   
+
+    <div id="InformacaoPessoalMedico">
+       
+        <img src="Styles/Images/medico.png"></img>
+    </div>
+    <div id="NoticiasPessoalMedico">
+  
+        <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+         <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+         <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+         <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+         <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+         <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+         <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+        
+         <div class="noticiaPessoal">
+            <h5>Noticia pessoal</h5>
+        </div>
+    </div>
+    <div id="EspecialidadesPessoalMedico">
+        Especialidades
+        
+        <div id="DiasConsultasPessoalMedico"></div>
+    </div>
+    
+
+</div>
+</div>
+
+
+<?php  $this->load->view('foot_Site.html');?>
