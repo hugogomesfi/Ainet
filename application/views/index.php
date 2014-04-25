0d5279e2b2a5e2d7f7808e72c659a7d53c33427c
HugoF <HugoF@Hugo>
2014-04-25 18:07:14 +0100

    Site a funcionar a tarde sem erros--Hugo

--- application/views/index.php
+++ application/views/index.php
@@ -2,17 +2,35 @@
 <?php  $this->load->view('Head');?>
 
 <script>
+    var $i=0;
 function mudaImagem(){
     
-    $('#tabelaimagem').css('background-image','+'+((scrolledY*0.8))+'px');
+    
+    var imgs=new Array("Styles/Images/imgfundo.jpg","Styles/Images/imgfundo2.jpg");
+    
+    
+    
+    if($i>=imgs.length){
+        $i=0;
+         
+    }
+    
+    
+    $('#imgfundo').fadeOut("slow");
+    $('#imgfundo').attr('src', imgs[$i]);
+    
+    $('#imgfundo').fadeIn("slow").delay(500);
+   
+    $i++;
 }
-
 </script>
 
-    <table id="tabelaimagem">
+    
         
-    <tr>
-        <td>
+        <img id="imgfundo" src="Styles/Images/imgfundo2.jpg"/>
+    
+        
+        <div id="publicacaoMedicos">
           <div class="publicacao" id="pub_1">
         <img src="Styles/Images/medicin_1.jpg"></img>
         <h5>Medicamentos</h5>
@@ -24,9 +42,9 @@ function mudaImagem(){
         <input type="button" value="Ler Mais" class="btnpub"></>
         </div>
     </div>  
-        </td>
         
-        <td>
+        
+       
            <div class="publicacao" id="pub_2">
          <img src="Styles/Images/medicin_2.jpg"></img>
          <h5>Novidades</h5>
@@ -37,9 +55,8 @@ function mudaImagem(){
         <input type="button" value="Ler Mais" class="btnpub"></>
         </div>
     </div> 
-        </td>
-        
-        <td>
+    
+       
             <div class="publicacao" id="pub_3">
          <img src="Styles/Images/medicin_3.jpg"></img>
          <h5>Genericos</h5>
@@ -50,19 +67,11 @@ function mudaImagem(){
         <input type="button" value="Ler Mais" class="btnpub"></>
         </div>
     </div> 
-        </td>
-    </tr>
-    
-</table>
+        
+    </div>
+   
 
-
-
-            
-            
-            
-            <ul class="bxslider" >
-                <li>
-                <div id="noticias">
+<div id="noticias">
             <div id="noticia1" class="noticia">
                 <h5>Titulo da Noticia</h5>
                 <div class="img_noticia">
@@ -73,7 +82,7 @@ function mudaImagem(){
                        
                     </p></div>
                 <div>
-                    <a href="">>ler mais</a>
+                    <a href="">ler mais</a>
                     
                 </div>
             </div>
@@ -86,7 +95,7 @@ function mudaImagem(){
                        
                     </p></div>
                 <div>
-                    <a href="">>ler mais</a>
+                    <a href="">ler mais</a>
                 </div>
             </div>
             <div id="noticia3"class="noticia">
@@ -99,7 +108,7 @@ function mudaImagem(){
                        
                     </p></div>
                 <div>
-                    <a href="">>ler mais</a>
+                    <a href="">ler mais</a>
                 </div>
             </div>
             <div id="noticia4"class="noticia">
@@ -114,89 +123,13 @@ function mudaImagem(){
                     </p>
                 </div>
                 <div>
-                    <a href="">>ler mais</a>
+                    <a href="">ler mais</a>
                 </div>
             </div>
             </div>
-                </li>
-                
-                
-                
-                
-                
-                
-                
-               <li>
-                <div id="noticias">
-            <div id="noticia1" class="noticia">
-                <h5>Titulo da Noticia</h5>
-                <div class="img_noticia">
-                    <img src="Styles/Images/147657108.jpg"></img>
-                </div>
-                <div class="resumo_noticia">
-                    <p>Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo. 
-                       
-                    </p></div>
-                <div>
-                    <a href="">>ler mais</a>
-                    
-                </div>
-            </div>
-            <div id="noticia2"class="noticia">
-                <h5>Noticia2</h5>
-                <div class="img_noticia">
-                    <img src="Styles/Images/Doenças do comportamento alimentar.jpg"></img>
-                </div>
-                <div class="resumo_noticia"><p>Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo. 
-                       
-                    </p></div>
-                <div>
-                    <a href="">>ler mais</a>
-                </div>
-            </div>
-            <div id="noticia3"class="noticia">
-                <h5>Noticia3</h5>
-                
-                <div class="img_noticia">
-                    <img src="Styles/Images/Meia Maratona.jpg"></img>
-                </div>
-                <div class="resumo_noticia"><p>Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo. 
-                       
-                    </p></div>
-                <div>
-                    <a href="">>ler mais</a>
-                </div>
-            </div>
-            <div id="noticia4"class="noticia">
-                <h5>Noticia4</h5>
-               
-                <div class="img_noticia">
-                    <img src="Styles/Images/abertura.jpg"></img>
-                </div>
-                <div class="resumo_noticia">
-                   <p>Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo. 
-                       
-                    </p>
-                </div>
-                <div>
-                    <a href="">>ler mais</a>
-                </div>
-            </div>
-            </div>
-                </li>  
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                <li>
-                <div id="noticias">
+
+
+             <div id="noticias">
             <div id="noticia1" class="noticia">
                 <h5>Publicação do medico</h5>
                 <div class="img_noticia">
@@ -252,10 +185,12 @@ function mudaImagem(){
                 </div>
             </div>
             </div>
-                
-                </li>
-                
-            </ul>
+        
+        
+        
+            
+            
+            
             
             
 
