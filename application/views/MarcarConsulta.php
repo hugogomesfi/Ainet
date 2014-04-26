5c57feb28d041c262beb6e1a523220c9e385a4be
Luis <Luis@Luis>
2014-04-26 08:07:36 +0100

    altrei marcar consulta.cuidados geriatricos e css. 26/4 as 7:30AM.  Luis  COM VALIDAÇAO DE ERROS

--- application/views/MarcarConsulta.php
+++ application/views/MarcarConsulta.php
@@ -11,22 +11,22 @@
 
                 <div class="formItem">
                     <label>Nome:</label>
-                    <input type="text" class="caixaTexto" placeholder="Insira o nome completo"required/>
+                    <input type="text" class="caixaTexto" placeholder="Insira o nome completo" required>
                 </div>
 
                 <div class="formItem">
                     <label>Data de Nascimento:</label>
-                    <input type="text" class="caixaTexto" placeholder="Insira a data de nascimento" required/>
+                    <input type="text" class="caixaTexto" placeholder="Insira a data de nascimento" required>
                 </div>
 
                 <div class="formItem">     
                     <label>Num seg social:</label>
-                    <input type="text" class="caixaTexto" placeholder="Insira o numero de seugrança social" required/>
+                    <input type="text" class="caixaTexto" placeholder="Insira o numero de seugrança social" required>
                 </div>
 
                 <div class="formItem"> 
                     <label>Morada:</label>
-                    <input type="text" class="caixaTexto" placeholder="Insira a sua morada" required/>
+                    <input type="text" class="caixaTexto" placeholder="Insira a sua morada" required>
                 </div>
 
 
@@ -35,21 +35,21 @@
 
                     <label>Sexo:</label>
 
-                    <input type="radio" name="sexo" value="masculino"/>
+                    <input type="radio" name="sexo" value="masculino">
                     <label>Masculino</label>
 
-                    <input type="radio" name="sexo" value="feminino" />
+                    <input type="radio" name="sexo" value="feminino" >
                     <label>Feminino</label>
                 </div>
                 <div class="formItem">
 
                     <label>Email:</label>
-                    <input type="email"  class="caixaTexto" placeholder="Insira o seu email" required/>
+                    <input type="email"  class="caixaTexto" placeholder="Insira o seu email" required>
                 </div>
 
                 <div class="formItem">
                     <label>Telefone:</label>
-                    <input type="number" class="caixaTexto" placeholder="Insira o seu numero de telefone" required/>
+                    <input type="number" class="caixaTexto" placeholder="Insira o seu numero de telefone" required>
                 </div>
 
 
@@ -66,19 +66,19 @@
 
                 <div class="formItem" >
                     <label>Especialidade:</label>
-                    <input type="text" class="caixaTexto" placeholder="Indique a especialidade que pretende" required/>
+                    <input type="text" class="caixaTexto" placeholder="Indique a especialidade que pretende" required>
                 </div>
                 <div class="formItem">
                     <label>Medico:</label>
-                    <input type="text" class="caixaTexto" placeholder="Indique um medico (opcional)" />
+                    <input type="text" class="caixaTexto" placeholder="Indique um medico (opcional)" >
                 </div>
                 <div class="formItem">
                     <label>Data:</label>
-                    <input type="date" class="caixaTexto" placeholder="Indique a data (opcional)" />
+                    <input type="date" class="caixaTexto"  >
                 </div>
                 <div class="formItem">
                     <label>Hora:</label>
-                    <input type="time" class="caixaTexto" placeholder="Insira a hora (opcional)" />
+                    <input type="time" class="caixaTexto"  >
                 </div>
 
             </fieldset>
@@ -86,7 +86,7 @@
 
 
             <div> 
-                <input type="submit" value="Marcar"/>
+                <input type="submit" value="Marcar">
             </div>
 
 
