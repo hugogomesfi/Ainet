f69d040e40e0a9c70c5fc7877e3b2cc26a8d7373
dg <dg@dg-PC>
2014-04-26 20:38:53 +0100

    creche pagina

---
+++ application/views/Creche.php
@@ -0,0 +1,42 @@
+<?php $this->load->view('Head'); ?>
+
+<div id="publicacaoMedicos">
+    <div class="publicacao" id="pub_1">
+        <img src="Styles/Images/medicin_1.jpg" alt="imagem de publicacao"/>
+        <h5>Medicamentos</h5>
+        <div class="resumo_noticia"><p>
+                Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo. 
+                Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo.       
+            </p></div>
+        <div>
+            <input type="button" value="Ler Mais" class="btnpub"/>
+        </div>
+    </div>  
+
+
+
+    <div class="publicacao" id="pub_2">
+        <img src="Styles/Images/medicin_2.jpg" alt="imagem de publicacao" />
+        <h5>Novidades</h5>
+        <div class="resumo_noticia"><p>Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo. 
+
+            </p></div>
+        <div>
+            <input type="button" value="Ler Mais" class="btnpub"/>
+        </div>
+    </div> 
+
+
+    <div class="publicacao" id="pub_3">
+        <img src="Styles/Images/medicin_3.jpg" alt="imagem de publicacao"/>
+        <h5>Genericos</h5>
+        <div class="resumo_noticia"><p>Lorem ipsum dolor sit amet,, imperdiet a, venenatis vitae, justo. 
+
+            </p></div>
+        <div>
+            <input type="button" value="Ler Mais" class="btnpub"/>
+        </div>
+    </div> 
+
+</div>
+<?php $this->load->view('foot_Site');
