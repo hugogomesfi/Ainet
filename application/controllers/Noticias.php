eb6e480d09e8d90a3c5b530100c5efe0625463ef
HugoF <HugoF@Hugo>
2014-06-14 15:07:03 +0100

    pagination

--- application/controllers/Noticias.php
+++ application/controllers/Noticias.php
@@ -5,14 +5,36 @@ class Noticias extends CI_Controller{
     public function __construct() {
         parent::__construct(); 
         $this->load->library('form_validation');
+        $this->load->library("pagination");
     }
     
     public function index() {
         $this->load->model('Noticia_model');
+         $config = array();
+        $config["base_url"] = base_url() . "Noticias";
+        $config["total_rows"] = $this->Noticia_model->getNumerodeNoticias();
+        $config["per_page"] = 2;
+        $config["uri_segment"] = 3;
+        $choice = $config["total_rows"] / $config["per_page"];
+        $config["num_links"] = round($choice);
+        
+        $this->pagination->initialize($config);
+        
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
         $data['view'] = 'Noticias';
-        $data['dados'] = $this->Noticia_model->getNoticiaPequena();
+        $data['dados'] = $this->Noticia_model->getNoticiaPequena($config["per_page"], $page);
+        $data["links"] = $this->pagination->create_links();
         $this->load->view('includes/template', $data);
+        
+        
     }
+    
+    
+        
+    
+    
+    
+    
         function insertNoticias() {
         if (!$this->session->userdata('user_id')) {
             
--- application/models/Noticia_model.php
+++ application/models/Noticia_model.php
@@ -3,20 +3,34 @@
 class Noticia_model extends CI_Model {
     public function __construct() {
         parent::__construct();
+         
     }
     
-    public function getNoticiaPequena(){
-        $query = $this->db->query("
-                SELECT publication.title,publication.abstract,publication.date,doctor.name
-                FROM `publication`,`scml_user`,`doctor`
-                where publication.updated_user_id=scml_user.person_id AND scml_user.id=doctor.user_id ");
-        $query = $query->result_array();
+    public function getNoticiaPequena($limit, $start){
+         $this->db->limit($limit, $start);
+         $query = $this->db->get("publication");
+//        $query = $this->db->query("
+//                SELECT publication.title,publication.abstract,publication.date,person.name
+//                FROM `publication`,`scml_user`,`person`
+//                where publication.updated_user_id=scml_user.person_id AND scml_user.person_id=person.id ");
         
-        return $query;
+          
+ 
+        if ($query->num_rows() > 0) {
+            $dados = $query->result_array();
+            return $dados;
+        }
+        return false;
+        
+        //return $dados;
     }
       public function insertNoticia($data) {
          
         $this->db->insert('publication', $data);
         return true;
+    }
+    
+    public function getNumerodeNoticias() {
+        return $this->db->count_all("publication");
     }
 }
