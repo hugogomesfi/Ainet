d0e2b29230f230e81d39a0bc041ea01053821e93
HugoF <HugoF@Hugo>
2014-06-15 15:25:19 +0100

    Alteraçoes home medico

--- application/views/HomeMedico.php
+++ application/views/HomeMedico.php
@@ -4,17 +4,17 @@
     <div id="homeMedico">
     <div id="InformacaoPessoalMedico">
         <img src="Styles/Images/medico.png" alt="foto medico" id="fotomedico"/>
-        <h4>Medico: João Crespo</h4>
-        <h4>Nome: João Antonio Constantino Crespo</h4>
-        <img src="Styles/Images/telefone.png" alt="telefone"/>
+        <h4>Dr. João Crespo</h4>
+        
+       
         <h4>Telefone: 9122345765</h4>
         <img src="Styles/Images/email.png" alt="email"/>
-        <h4>Joao.Crespo@hotnail.com</h4>
-        <a href="mailto:example@aaaaa.com">Enviar Email </a>
-        <p><button class="btn btn-success " style="width: 100%;"  onclick="location.href='<?php echo site_url();?>/HomeMedico'">Noticias</button></p>
-        <p><button class="btn btn-primary " style="width: 100%;"  onclick="location.href='<?php echo site_url();?>/#'">Alterar Dados</button></p>
+   
+        <a href="mailto:Joao.Crespo@hotnail.com">Joao.Crespo@hotnail.com</a>
+        <p><button class="btn btn-success " style="width: 100%;"  onclick="location.href='<?php echo site_url();?>/HomeMedico'">Minhas Noticias</button></p>
+        <p><button class="btn btn-primary " style="width: 100%;"  onclick="location.href='<?php echo site_url();?>/#'">Alterar Dados Pessoais</button></p>
         <p><button class="btn btn-primary " style="width: 100%;"  onclick="location.href='<?php echo site_url();?>/ComporNoticia'">Criar Noticia</button></p>       
-        <p><button class="btn btn-primary " style="width: 100%;"  onclick="location.href='<?php echo site_url();?>/PedidosConsultas'">Consultas</button></p>
+        <p><button class="btn btn-primary " style="width: 100%;"  onclick="location.href='<?php echo site_url();?>/PedidosConsultas'">Horarios</button></p>
         
         
     </div>
