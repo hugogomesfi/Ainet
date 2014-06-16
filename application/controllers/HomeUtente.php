898f3230e1736ada2f773c8fb6b658a5557777a0
HugoF <HugoF@Hugo>
2014-06-16 18:21:38 +0100

    Noticias pagina principal ok

--- application/controllers/HomeUtente.php
+++ application/controllers/HomeUtente.php
@@ -1,6 +1,6 @@
 <?php
 
-class HomeMedico extends CI_Controller{
+class HomeUtente extends CI_Controller{
     
     public function __construct() {
         parent::__construct(); 
@@ -8,13 +8,9 @@ class HomeMedico extends CI_Controller{
     
     public function index() {
         $this->load->model('Noticia_model');
-        $data['view'] = 'HomeMedico';
-        $this->load->model('Medico_m');
-        $idmedico=$this->session->userdata('user_id');
-        
-        $data['especialidades'] =$this->Medico_m->getEspecialidadesMedico($idmedico);
-        
-        $data['noticias'] =$this->Noticia_model->getNoticiasMedico();
+        $data['view'] = 'HomeUtente';
+         $idUtente=$this->session->userdata('user_id');
+       
         
         $this->load->view('includes/template', $data);
     }
