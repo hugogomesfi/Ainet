ff87eca5b3ded79fee2764db1e721dc186d8c326
HugoF <HugoF@Hugo>
2014-06-18 13:22:49 +0100

    listagem consultas administrador

--- application/views/Noticias.php
+++ application/views/Noticias.php
@@ -1,67 +1,66 @@
 <div id="contentor">
-    
+
     <h1>Notícias</h1>  
-<?php
+    <?php
 //var_dump($dados);
 
-foreach ($dados as $value) {
-    
+    foreach ($dados as $value) {
 
 
-echo "<div class=\"noticias panel panel-primary\">\n"; 
-echo "    <div class=\"noticiasEsq panel-heading\">\n"; 
-echo "        <div class=\"noticeTitle\">\n"; 
-echo "            <h4> ".$value->title."</h4>\n"; 
-echo "        </div>\n"; 
-echo "        <a href=\"<?php echo site_url('/NoticiaGrande/index');?>\">\n"; 
-echo "            <img src=\"Styles/Images/n_.jpg\" alt=\"imagem de noticia\"/>\n"; 
-echo "        </a>\n"; 
-echo "        <div class=\"noticeDate\">\n"; 
-echo "            <p> ".$value->date." </p>\n"; 
+
+        echo "<div class=\"noticias panel panel-primary\">\n";
+        echo "    <div class=\"noticiasEsq panel-heading\">\n";
+        echo "        <div class=\"noticeTitle\">\n";
+        echo "            <h4> " . $value->title . "</h4>\n";
+        echo "        </div>\n";
+        echo "        <a href=\"<?php echo site_url('/NoticiaGrande/index');?>\">\n";
+        echo "            <img src=\"Styles/Images/n_.jpg\" alt=\"imagem de noticia\"/>\n";
+        echo "        </a>\n";
+        echo "        <div class=\"noticeDate\">\n";
+        echo "            <p> " . $value->date . " </p>\n";
 //echo "            <span>".$value['name']."</span>\n"; 
-echo "        </div>\n"; 
-echo "    </div>\n"; 
-echo "\n"; 
-echo "    <div class=\"noticiasDir\">\n"; 
-echo "        <div class=\"noticeSpecialidade\">\n"; 
-echo "            <h6>Ginecologia</h6>\n"; 
-echo "            <div class=\"linhaHugo\"></div>\n"; 
-echo "        </div>\n"; 
-echo "        <div class=\"noticeText\">\n"; 
-echo "            <span>  ".$value->abstract."  </span>\n"; 
-echo "        </div>\n"; 
-echo "        <div class=\"noticeAuthor\">\n"; 
+        echo "        </div>\n";
+        echo "    </div>\n";
+        echo "\n";
+        echo "    <div class=\"noticiasDir\">\n";
+        echo "        <div class=\"noticeSpecialidade\">\n";
+        echo "            <h6>Ginecologia</h6>\n";
+        echo "            <div class=\"linhaHugo\"></div>\n";
+        echo "        </div>\n";
+        echo "        <div class=\"noticeText\">\n";
+        echo "            <span>  " . $value->abstract . "  </span>\n";
+        echo "        </div>\n";
+        echo "        <div class=\"noticeAuthor\">\n";
 
-echo "           \n"; 
-?>
-   
- <form method="get" action="<?=site_url('Noticias/encontraNoticia');?>">
-     
-<?php
-$databtn = array(
-    'name' => 'noticia',
-    'class' => 'btn btn-primary btn-sm',
-    'value' => "$value->id",
-    'type' => 'submit',
-    'style' => 'position: absolute; right:10px;',
-    'content' => 'Ler Mais'
-);
+        echo "           \n";
+        ?>
 
-echo form_button($databtn);
-  ?> 
-</form>
-<?php
-echo "            \n"; 
-echo "        </div>\n"; 
-echo "    </div>\n"; 
-echo "</div>\n";
-}
+        <form method="get" action="<?= site_url('Noticias/encontraNoticia'); ?>">
 
-?>
-   
+            <?php
+            $databtn = array(
+                'name' => 'noticia',
+                'class' => 'btn btn-primary btn-sm',
+                'value' => "$value->id",
+                'type' => 'submit',
+                'style' => 'position: absolute; right:10px;',
+                'content' => 'Ler Mais'
+            );
+
+            echo form_button($databtn);
+            ?> 
+        </form>
+        <?php
+        echo "            \n";
+        echo "        </div>\n";
+        echo "    </div>\n";
+        echo "</div>\n";
+    }
+    ?>
+
     <div class="links">
-        
-    <p><?php echo $links; ?></p>
+
+        <p><?php echo $links; ?></p>
     </div>
 
 
